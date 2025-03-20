2d871df2014e380fad776923ac2d07845521602a
himankBellpost <himank@bellpost.com>
2025-03-21 01:22:15 +0530

    functional component

---
+++ src/component/Greet.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Greet(){
+    return <h1>Hello HIMANK</h1>
+}
+
+const Greet =  ()=> <h1>Hello himank</h1>
+
+
+export default Greet 
+
+
+// named export 
+// export const Greet =  ()=> <h1>Hello himank</h1>
+
+
