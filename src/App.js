a564487efdc78de7122ed0ca5b022279c4f8e69c
himankBellpost <himank@bellpost.com>
2025-03-22 01:39:57 +0530

    jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
-import Greet  from './component/Greet';
-import Welcome from './component/Welcome';
-// import {Greet}  from './component/Greet';
-
+import './App.css'
+import Greet  from './component/1_Greet'
+// import {Greet}  from './component/Greet'; // named import
+import Welcome from './component/2_Welcome'
+import Hello from './component/3_hello'
 
 function App() {
   return (
     <div className="App">
       <Greet/>
       <Welcome/>
+      <Hello/>
     </div>
   );
 }
